9aabd35765f0eaa5793833d5d6143fc9067b2682
User <User@DESKTOP-NJN53B5>
2022-03-22 19:02:49 +0600

    business_unit edit modal set value

--- pim-admin/src/main/resources/static/js/custom.js
+++ pim-admin/src/main/resources/static/js/custom.js
@@ -13,22 +13,19 @@ function loadCompanyEditData(el, dd){
 		editModal.querySelector('#phoneNo').value = parentNode.querySelector('#cPhoneNo').value;
 		editModal.querySelector('#email').value = parentNode.querySelector('#cEmail').value;
 		editModal.querySelector('#website').value = parentNode.querySelector('#cWebsite').value;
+		editModal.querySelector('#status').value = parentNode.querySelector('#cStatus').value;
 			
 }
 
 function loadBusinessUnitEditData(el, dd){	
-	var editModal = document.getElementById('editCompanyModal');
+	var editModal = document.getElementById('editBusinessUnitModal');
 	var parentNode = el.parentNode.parentNode.parentNode.parentNode;
 	
 	editModal.querySelector('#id').value = parentNode.querySelector('#cId').value;
-	editModal.querySelector('#name').value = parentNode.querySelector('#cName').value;
-	editModal.querySelector('#bankId').value = parentNode.querySelector('#cBankId').value;
-	editModal.querySelector('#address').value = parentNode.querySelector('#cAddress').value;
-	editModal.querySelector('#city').value = parentNode.querySelector('#cCity').value;
-	editModal.querySelector('#state').value = parentNode.querySelector('#cState').value;
-	editModal.querySelector('#country').value = parentNode.querySelector('#cCountry').value;
-	editModal.querySelector('#phoneNo').value = parentNode.querySelector('#cPhoneNo').value;
-	editModal.querySelector('#email').value = parentNode.querySelector('#cEmail').value;
-	editModal.querySelector('#website').value = parentNode.querySelector('#cWebsite').value;
+	editModal.querySelector('#unitName').value = parentNode.querySelector('#cUnitName').value;
+	editModal.querySelector('#shortName').value = parentNode.querySelector('#cShortName').value;
+	editModal.querySelector('#companyName').value = parentNode.querySelector('#cCompanyName').value;
+	editModal.querySelector('#hierarchy').value = parentNode.querySelector('#cHierarchy').value;
+	editModal.querySelector('#status').value = parentNode.querySelector('#cStatus').value;
 		
 }
