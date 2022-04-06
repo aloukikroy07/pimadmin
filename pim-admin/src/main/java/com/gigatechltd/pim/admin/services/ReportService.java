fb9f33d22b1f918023daa61db6313a5edd5a0b5d
User <User@DESKTOP-ETNQBOE>
2022-04-06 14:32:21 +0600

    aaa

--- pim-admin/src/main/java/com/gigatechltd/pim/admin/services/ReportService.java
+++ pim-admin/src/main/java/com/gigatechltd/pim/admin/services/ReportService.java
@@ -57,7 +57,7 @@ public class ReportService {
 		try {
 			List<CustomerProfile> customerProfileDatoDateta = reportRepository.getCustomerProfile(request, cp);
 			//jrbcd = new JRBeanCollectionDataSource(customerProfileData);
-			template = ResourceUtils.getFile("classpath:CustomerReport.jrxml");
+			template = ResourceUtils.getFile("classpath:DatewiseUserRegistration.jrxml");
 			JasperReport jasperReport = JasperCompileManager.compileReport(template.getAbsolutePath());
 			Map<String, Object> parameters = new HashMap<>();
 		    parameters.put("fromDate", fromDate);
