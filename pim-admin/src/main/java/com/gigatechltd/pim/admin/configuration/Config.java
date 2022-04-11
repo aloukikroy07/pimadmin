f873afd825ed568017ab3b300c81a22f74891ca2
User <User@DESKTOP-NJN53B5>
2022-04-11 12:27:08 +0600

    logger

--- pim-admin/src/main/java/com/gigatechltd/pim/admin/configuration/Config.java
+++ pim-admin/src/main/java/com/gigatechltd/pim/admin/configuration/Config.java
@@ -3,29 +3,34 @@ package com.gigatechltd.pim.admin.configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
+	private static final Logger logger = LoggerFactory.getLogger(Config.class);
     public static final int STATUS_PENDING = 0;
     public static final int STATUS_ACTIVE = 1;
+    
     
     @Bean
     public Connection getDataSource() throws SQLException, ClassNotFoundException {
     	Connection connection = null ;
     	try {
 			Class.forName("com.oracle.jdbc.Driver");
-		} catch (ClassNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			logger.error("In getDataSource method in Config class : "+e);
 		}
 		try {
 			connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.70.55:1521/APEXDB.GIGATECHLTD.COM?user=pimuser&password=pimuser");
-		} catch (SQLException e1) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
+			logger.error("In getDataSource method in Config class : "+e);
 		}
         return connection;
     }
--- pim-admin/src/main/java/com/gigatechltd/pim/admin/controller/ReportController.java
+++ pim-admin/src/main/java/com/gigatechltd/pim/admin/controller/ReportController.java
@@ -26,11 +26,21 @@ public class ReportController {
 	
 	@GetMapping({"/customer/report"})
 	public String openCustomerReportParameterWindow(Model model) {
-		return "report/customerReport";
+		return "report/customerRegistrantionReport";
 	}
 	
 	@PostMapping({"/customer/report/generate"})
 	public void generateCustomerReport(HttpServletRequest request, HttpServletResponse response, CustomerProfile cp) throws IOException, ClassNotFoundException, SQLException {
+		reportService.exportJasperReport(request, response, cp);
+	}
+	
+	@GetMapping({"/datewise/trans/report"})
+	public String openDatewiseTransReport(Model model) {
+		return "report/datewiseTransReport";
+	}
+	
+	@PostMapping({"/datewise/trans/report/generate"})
+	public void generateDatewiseTransReport(HttpServletRequest request, HttpServletResponse response, CustomerProfile cp) throws IOException, ClassNotFoundException, SQLException {
 		reportService.exportJasperReport(request, response, cp);
 	}
 	
